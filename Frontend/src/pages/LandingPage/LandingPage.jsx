3907626df199d32d7edc259082e7718947de0e4a
codedeprived <smanmohan735@gmail.com>
2024-11-12 14:03:11 +0530

    testing deployment ready name change

--- Frontend/src/pages/LandingPage/LandingPage.jsx
+++ Frontend/src/pages/LandingPage/LandingPage.jsx
@@ -15,7 +15,7 @@ function LandingPage() {
   
         {/* Frosted Glass Content */}
         <div className="relative z-10 text-center text-white bg-white bg-opacity-10 backdrop-blur-lg p-8 rounded-xl shadow-2xl border border-white border-opacity-30 font-roboto ">
-          <h1 className="text-4xl font-bold mb-4">Welcome to Our Fleet Management System</h1>
+          <h1 className="text-4xl font-bold mb-4">Welcome to Our Perfect Fleet Management System</h1>
           <p className="text-xl mb-8">Efficiently manage your fleet with ease.</p>
           
           {/* Buttons */}
