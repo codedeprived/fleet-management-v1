33385205c889fccfd31d1d53e879aad726c60efb
vishalsharma123321 <vishalsharma2212003@gmail.com>
2025-01-02 23:49:01 +0530

    changed the cors in the server.js to access the the port 5175

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'] // Add this line to allow requests from your frontend
+  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175'] // Add this line to allow requests from your frontend
 }));
 
 app.use(bodyParser.json());
