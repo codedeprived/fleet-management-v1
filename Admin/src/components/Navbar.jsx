c24570002d5e16d7edb492fc8f5ec7363ae2a385
vishalsharma123321 <vishalsharma2212003@gmail.com>
2025-03-12 22:40:43 +0530

    deleted the flow bit logo which was causing problem between the perfect fleet logo .

--- Admin/src/components/Navbar.jsx
+++ Admin/src/components/Navbar.jsx
@@ -30,11 +30,7 @@ const Navbar = ({ isDarkMode, toggleDarkMode, isOpen, setIsOpen }) => {
                 </svg>
               </button>
               <a href="/dashborad" className="flex ms-2 md:me-24">
-                <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  className="h-8 me-3"
-                  alt="FlowBite Logo"
-                />
+              
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
                   FAST FLEET 
                 </span>
